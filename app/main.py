9dd81d036168d6da2a90ba6d31110e9bd11d2a9b
Jairo Andre <jairo.calagua.m@uni.pe>
2025-07-17 08:49:13 -0500

    main.py finalizado

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from src.modelo_seq2seq import Seq2SeqModel
 
@@ -8,8 +12,8 @@ texto = st.text_area("Introduce un texto en Español o Inglés:", height=150)
 
 if st.button("Traducir con ambos modelos"):
     with st.spinner("Cargando modelos..."):
-        modelo_full = Seq2SeqModel("t5-small", "full_finetune")
-        modelo_lora = Seq2SeqModel("t5-small", "lora")
+        modelo_full = Seq2SeqModel(mode="full_finetune")
+        modelo_lora = Seq2SeqModel(mode="lora")
     
     st.subheader("Resultados de Traducción")
     col1, col2 = st.columns(2)
@@ -25,7 +29,7 @@ if st.button("Traducir con ambos modelos"):
         st.success(resultado2)
     
     st.subheader("Comparación")
-    if resultado1 == resultado2:
+    if resultado1.strip().lower() == resultado2.strip().lower():
         st.info("Ambos modelos generaron la misma traducción.")
     else:
         st.markdown("- Fine-tuning completo puede ser más preciso pero consume más recursos.")
